9cbe0df8181c61f710686dbd7908392f65c3271f
FromagePatin <76440058+FromagePatin@users.noreply.github.com>
2023-09-06 15:08:38 +0200

    Add prim directory + ch.sv

---
+++ hw/prim/ch.sv
@@ -0,0 +1,17 @@
+// ------------------------------------------------------
+//  Module name: prim_generic_ch
+//  Description: Choose function
+// ------------------------------------------------------
+
+module prim_generic_ch #(
+    parameter int unsigned Width = 32
+) (
+    input  logic [Width-1:0] in0_i,
+    input  logic [Width-1:0] in1_i,
+    input  logic [Width-1:0] in2_i,
+    output logic [Width-1:0] ch_o
+);
+
+    assign ch_o = (in0_i & in1_i) ^ (in0_i & in2_i);
+
+endmodule
