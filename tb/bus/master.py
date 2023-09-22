02e135f862444f5832040e61a0c72d20977f1639
teobiton <teo.biton@gmail.com>
2023-09-22 11:38:25 +0200

    Add register accesses cocotb tests
    This commit introduces the required components to verify the
    register accesses using cocotb.
    
    A master interface relying on cocotb-bus is defined to simplify
    the read and write operations and make them repeatable. It is
    extendable to support a toggling of the strobe signal.
    
    The actual test is built with random values and valid addresses
    sent to the register interface. The status is pass with this test
    only.
    
    More tests can be added in the future to verify the strobe
    behavior, the error signal when invalid addresses are sent, and
    finally run the test for different DataWidth and ByteAlign values
    using pytest.

---
+++ tb/bus/master.py
@@ -0,0 +1,162 @@
+"""Driver for a simple bus interface
+
+The interface is inspired from APB but its size is generic and thus not
+100% compliant. The master is really simple and allows read and write
+operations.
+
+"""
+
+from typing import Dict, List, Optional
+
+from cocotb.clock import Clock
+from cocotb.decorators import coroutine
+from cocotb.triggers import RisingEdge
+from cocotb.binary import BinaryValue
+from cocotb.handle import SimHandleBase
+
+from cocotb_bus.drivers import BusDriver
+
+
+class Mapper(BusDriver):
+    """Interface Mapper
+
+    This class maps the bus signals to the actual entity signals.
+
+    """
+
+    signals: List[str] = [
+        "reqdata",
+        "reqaddr",
+        "reqvalid",
+        "reqwrite",
+        "reqready",
+        "reqstrobe",
+        "rspready",
+        "rspvalid",
+        "rspdata",
+        "rsperror",
+    ]
+
+    def __init__(
+        self,
+        entity: SimHandleBase,
+        name: str,
+        clock: Clock,
+        mapping: Optional[Dict[str, str]] = None,
+        **kwargs
+    ):
+        self._signals = mapping if mapping is not None else self.signals
+
+        BusDriver.__init__(self, entity, name, clock, **kwargs)
+
+        # Drive requests signals to default values
+        self.bus.reqdata.value.binstr = "0" * len(self.bus.reqdata)
+        self.bus.reqaddr.value.binstr = "0" * len(self.bus.reqaddr)
+        self.bus.reqvalid.setimmediatevalue(0)
+        self.bus.reqwrite.setimmediatevalue(0)
+        self.bus.reqstrobe.value.binstr = "0" * len(self.bus.reqstrobe)
+
+        # Master is always ready
+        self.bus.rspready.setimmediatevalue(1)
+
+    def read(self, address: int):
+        pass
+
+    def write(self, address: int, value: int):
+        pass
+
+
+class Master(Mapper):
+    """Master interface"""
+
+    def __init__(
+        self,
+        entity: SimHandleBase,
+        name: str,
+        clock: Clock,
+        mapping: Optional[Dict[str, str]] = None,
+        **kwargs
+    ):
+        # Workaround for an issue with Verilator/Cocotb (see https://github.com/cocotb/cocotb/issues/3259)
+        # case_insensitive=False is needed otherwise the bus class is unusable
+        Mapper.__init__(
+            self, entity, name, clock, mapping, case_insensitive=False, **kwargs
+        )
+
+    def __len__(self) -> int:
+        return 2 ** len(self.bus.address)
+
+    @coroutine
+    async def read(self, address: int) -> BinaryValue:
+        """Issue a request to the bus and block until this comes back.
+
+        Simulation time still progresses
+        but syntactically it blocks.
+
+        Args:
+            address: The address to read from.
+            sync: Wait for rising edge on clock initially.
+                Defaults to True.
+
+        Returns:
+            The read data value.
+
+        """
+
+        await self._acquire_lock()
+
+        await RisingEdge(self.clock)
+
+        self.bus.reqaddr.value = address
+        self.bus.reqwrite.value = 0
+        self.bus.reqvalid.value = 1
+        self.bus.reqstrobe.value = int("1" * len(self.bus.reqstrobe), 2)
+
+        await RisingEdge(self.clock)
+
+        self.bus.reqaddr.value.binstr = "0" * len(self.bus.reqaddr)
+        self.bus.reqvalid.value = 0
+        self.bus.reqstrobe.value = int("0" * len(self.bus.reqstrobe), 2)
+
+        while True:
+            if self.bus.rspvalid.value:
+                break
+            await RisingEdge(self.clock)
+
+        # Get the response data
+        data = self.bus.rspdata.value
+
+        self._release_lock()
+        return data
+
+    @coroutine
+    async def write(self, address: int, value: int) -> None:
+        """Write request to the given address with the specified value.
+
+        Args:
+            address: The address to write to.
+            value: The data value to write.
+
+        """
+
+        await self._acquire_lock()
+
+        # Apply values to bus
+        await RisingEdge(self.clock)
+
+        self.bus.reqaddr.value = address
+        self.bus.reqdata.value = value
+        self.bus.reqwrite.value = 1
+        self.bus.reqvalid.value = 1
+        self.bus.reqstrobe.value = int("1" * len(self.bus.reqstrobe), 2)
+
+        # Deassert write
+        await RisingEdge(self.clock)
+
+        self.bus.reqaddr.value.binstr = "0" * len(self.bus.reqaddr)
+        self.bus.reqdata.value.binstr = "0" * len(self.bus.reqdata)
+        self.bus.reqwrite.value = 0
+        self.bus.reqvalid.value = 0
+        self.bus.reqstrobe.value = int("0" * len(self.bus.reqstrobe), 2)
+
+        self._release_lock()
---
+++ tb/lib.py
@@ -0,0 +1,54 @@
+import cocotb
+from cocotb.triggers import Timer
+
+from typing import Dict
+
+
+SHA_MAPPING: Dict[str, str] = {
+    "reqdata": "sha_s_reqdata_i",
+    "reqaddr": "sha_s_reqaddr_i",
+    "reqvalid": "sha_s_reqvalid_i",
+    "reqwrite": "sha_s_reqwrite_i",
+    "reqready": "sha_s_reqready_o",
+    "reqstrobe": "sha_s_reqstrobe_i",
+    "rspready": "sha_s_rspready_i",
+    "rspvalid": "sha_s_rspvalid_o",
+    "rspdata": "sha_s_rspdata_o",
+    "rsperror": "sha_s_rsperror_o",
+}
+
+
+def align(addr: int, bytealign: bool):
+    step = 8 if bytealign else 32
+    return int(addr / step)
+
+
+@cocotb.coroutine
+async def init(sha):
+    """Initialize input signals value"""
+
+    """ 
+
+    This would be the correct way to do it.
+    But verilator does not support it so the list must be maintained by hand.
+
+    for signal in dir(sha):
+        if signal.endswith('_i') and signal != "clk_i":
+            print(f"signal found = {signal}")
+            sha._id(signal, extended=False).value = 0 
+            
+    """
+
+    sha.sha_process_i.value = 0
+    sha.sha_digestack_i.value = 0
+
+    sha.sha_s_reqdata_i.value = 0
+    sha.sha_s_reqaddr_i.value = 0
+    sha.sha_s_reqvalid_i.value = 0
+    sha.sha_s_reqwrite_i.value = 0
+    sha.sha_s_reqstrobe_i.value = 0
+    sha.sha_s_rspready_i.value = 0
+
+    sha.rst_ni.value = 0
+
+    await Timer(1, units="ns")
