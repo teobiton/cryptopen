8f5f43b9bbed6e1690747d2a893767331e84081e
teobiton <teo.biton@gmail.com>
2023-11-12 15:31:00 +0100

    Standardize sha256 model and tests with sha512
    The model and test construction is improved with the way digest
    widths are handled.

--- tb/sha2/model/test_sha256.py
+++ tb/sha2/model/test_sha256.py
@@ -1,56 +1,38 @@
 from sha256 import sha256
+import pytest
 
 
-def test_one_block_message() -> None:
+@pytest.mark.parametrize("digest_width", [224, 256])
+def test_one_block_message(digest_width) -> None:
     """One-block message from FIPS sha256 examples"""
 
     message: str = "abc"
 
-    sha256_model = sha256(debug=True, encoding="ascii")
+    hash_core = sha256(digest_width=digest_width, debug=True, encoding="ascii")
 
-    sha256_model.process(message)
-    sha256_model._dbg_digest()
+    hash_core.process(message)
 
-    assert (
-        sha256_model.digest()
-        == "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
-    )
+    expected_digest = {
+        256: "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
+        224: "23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7",
+    }
 
-    sha224_model = sha256(sha224=True, debug=True, encoding="ascii")
-
-    sha224_model.process(message)
-
-    assert (
-        sha224_model.digest()
-        == "23097d223405d8228642a477bda255b32aadbce4bda0b3f7e36c9da7"
-    )
+    assert hash_core.digest() == expected_digest[digest_width]
 
 
-def test_multi_block_message() -> None:
+@pytest.mark.parametrize("digest_width", [224, 256])
+def test_multi_block_message(digest_width) -> None:
     """Multi-block message from FIPS sha256 examples"""
 
     message: str = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
 
-    sha256_model = sha256(debug=True, encoding="ascii")
+    hash_core = sha256(digest_width=digest_width, debug=True, encoding="ascii")
 
-    sha256_model.process(message)
-    sha256_model._dbg_digest()
+    hash_core.process(message)
 
-    assert (
-        sha256_model._dbg_hash_values[0]
-        == "85e655d6417a17953363376a624cde5c76e09589cac5f811cc4b32c1f20e533a"
-    )
+    expected_digest = {
+        256: "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1",
+        224: "75388b16512776cc5dba5da1fd890150b0c6455cb4f58b1952522525",
+    }
 
-    assert (
-        sha256_model.digest()
-        == "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"
-    )
-
-    sha224_model = sha256(sha224=True, encoding="ascii")
-
-    sha224_model.process(message)
-
-    assert (
-        sha224_model.digest()
-        == "75388b16512776cc5dba5da1fd890150b0c6455cb4f58b1952522525"
-    )
+    assert hash_core.digest() == expected_digest[digest_width]
