78a1b126530240544ad077cba03acfa7b5975174
teobiton <teo.biton@gmail.com>
2024-01-03 18:57:05 +0100

    Use Tcl arguments to build Vivado project
    Instead of using environment variables in the Tcl scripts, use
    -tclargs command-line option from Vivado and parse the arguments
    in the Tcl scripts.
    
    RTL parameters values are also given to the run.tcl script through
    this option.
    
    Fixes #62

--- fpga/scripts/project.tcl
+++ fpga/scripts/project.tcl
@@ -4,7 +4,25 @@
 
 set project cryptopen
 
-create_project $project . -force -part $::env(XILINX_PART)
-set_property board_part $::env(XILINX_BOARD) [current_project]
+# Default part and board values
+set part  xc7s100fgga676-2
+set board xilinx.com:sp701:part0:1.0
+
+# Process tclargs field
+set num_arg $argc
+if {$num_arg > 0} {
+    for {set i 0} {$i < $num_arg} {incr i} {
+        set arg [lindex $argv $i]
+        if {[regexp {^part=(.*)$} $arg match val] == 1} {
+            set part $val
+        }
+        if {[regexp {^board=(.*)$} $arg match val] == 1} {
+            set board $val
+        }
+    }
+}
+
+create_project $project . -force -part $part
+set_property board_part $board [current_project]
 
 set_param general.maxThreads 8
