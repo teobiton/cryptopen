b562ced8f6dab59a7381c8c78ca5f42aab2e0ce3
teobiton <teo.biton@gmail.com>
2024-01-07 12:15:36 +0100

    Add drivers to run algorithms
    Driver functions rely on coroutines to access registers and
    control the algorithm. A test for one-block messages is added for
    each ip.

--- tb/test_tops.py
+++ tb/test_tops.py
@@ -3,24 +3,60 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import os
-from secrets import choice, randbits
-from typing import Dict, List
+from typing import Dict, List, Union
 
 import cocotb
 import pytest
 from cocotb.clock import Clock
-from cocotb.regression import TestFactory
-from cocotb.result import TestSuccess
 from cocotb.runner import Simulator, get_runner
-from cocotb.triggers import ClockCycles, RisingEdge, Timer
+from cocotb.triggers import RisingEdge, Timer
 
-from interface.bus.master import Master
-from interface.utils import CTRL_ADDR, DIGEST_ADDR, BLOCK_ADDR
-from interface.utils import MAPPING
+from driver import Driver
+from sha1.model.sha1_model import sha1
+from sha2.model.sha256_model import sha256
+from sha2.model.sha512_model import sha512
 
 SIM = os.getenv("SIM", "verilator")
 SIM_BUILD = os.getenv("SIM_BUILD", "sim_build")
 WAVES = os.getenv("WAVES", "0")
+
+if cocotb.simulator.is_running():
+    DATA_WIDTH = int(cocotb.top.DataWidth)
+    ADDR_WIDTH = int(cocotb.top.AddrWidth)
+    BLOCK_WIDTH = int(cocotb.top.BlockWidth)
+    BYTE_ALIGN = int(cocotb.top.ByteAlign)
+    DIGEST_WIDTH = int(cocotb.top.DigestWidth)
+
+MAPPING: Dict[str, str] = {
+    "reqdata": "sha_s_reqdata_i",
+    "reqaddr": "sha_s_reqaddr_i",
+    "reqvalid": "sha_s_reqvalid_i",
+    "reqwrite": "sha_s_reqwrite_i",
+    "reqready": "sha_s_reqready_o",
+    "reqstrobe": "sha_s_reqstrobe_i",
+    "rspready": "sha_s_rspready_i",
+    "rspvalid": "sha_s_rspvalid_o",
+    "rspdata": "sha_s_rspdata_o",
+    "rsperror": "sha_s_rsperror_o",
+}
+
+
+def intblock(blocks: List[bytearray], index: int) -> int:
+    return int.from_bytes(blocks[index], byteorder="big")
+
+
+# Factory to build models on the fly
+def core_factory(
+    name: str, block_width: int, digest_width: int
+) -> Union[sha1, sha256, sha512]:
+    if name == "sha1":
+        return sha1()
+    elif name == "sha256":
+        return sha256(digest_width=digest_width)
+    elif name == "sha512":
+        return sha512(digest_width=digest_width)
+    else:
+        return ValueError(f"Unsupported block width: {block_width}")
 
 
 @cocotb.coroutine
@@ -53,8 +89,66 @@ async def toggle_reset(dut) -> None:
     assert dut.rst_ni.value == 1, f"{dut.name} is still under reset"
 
 
+@cocotb.test()
+async def run_algorithm(dut) -> None:
+    """Write block and run algorithm for a one block message"""
+
+    await init(dut)
+
+    cocotb.start_soon(Clock(dut.clk_i, period=10, units="ns").start())
+
+    # Turn off reset
+    dut.rst_ni.value = 1
+
+    await Timer(35, units="ns")
+
+    assert dut.rst_ni.value == 1, f"{dut.name} is still under reset"
+
+    # Instantiate driver to access registers
+    driver: Driver = Driver(
+        entity=dut,
+        data_width=DATA_WIDTH,
+        addr_width=ADDR_WIDTH,
+        byte_align=BYTE_ALIGN,
+        block_width=BLOCK_WIDTH,
+        digest_width=DIGEST_WIDTH,
+        bus_mapping=MAPPING,
+    )
+
+    message: str = "abc"
+
+    # Instantiate Python model
+    model = core_factory(dut.name, BLOCK_WIDTH, DIGEST_WIDTH)
+    model.process(message)
+
+    # Write block to registers
+    block = intblock(model.blocks, 0)
+    await driver.write_block(block=block)
+
+    await RisingEdge(dut.clk_i)
+
+    # Check if the core received the correct value
+    assert dut.sha_block.value == block
+
+    # Enable algorithm by writing enable bit in control register
+    await driver.enable()
+
+    # Read valid bit in control register
+    while True:
+        core_done = await driver.read_valid()
+        if core_done:
+            break
+        await RisingEdge(dut.clk_i)
+
+    # Read digest value
+    digest: str = f"{await driver.read_digest():x}"
+    assert digest == model.digest().lstrip(
+        "0"
+    ), f"Expected digest {model.digest().lstrip('0')}, got {digest}"
+
+
 @pytest.mark.parametrize("ip", ["sha1", "sha256", "sha512"])
-def test_IP(ip):
+def test_ip(ip):
     """Run cocotb tests on accelators IPs."""
 
     tests_dir: str = os.path.dirname(__file__)
